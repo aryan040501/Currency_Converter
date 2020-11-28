ebfde6f3de5bb016b53b9ed8713961bb90c3868a
yasharth328 <57102314+yasharth328@users.noreply.github.com>
2020-11-28 09:06:23 -0800

    Update 1.0
    Did some minor Changes.

--- Currency_Converter.py
+++ Currency_Converter.py
@@ -1,4 +1,8 @@
+#Currency Convertor
+
+
 import tkinter as tk
+import tkinter.ttk as ttk
 from forex_python.converter import CurrencyRates
 
 def show_tax(rate):
@@ -10,41 +14,42 @@ def show_tax(rate):
 	net.set(format(a, '.2f'))
 
 root = tk.Tk()
-root.geometry('350x350')
+root.geometry('400x440')
 root.title('Currency Converter')
+#root.configure(bg='light blue')
 
 amt = tk.IntVar()
 cf = tk.StringVar()
 ct = tk.StringVar()
 net = tk.StringVar()
 
-tk.Label(root, text='Input amount').grid(row=0, column=0, columnspan=5)
+ttk.Label(root, text='Input amount', font='Times').grid(row=0, column=0, columnspan=5)
 
-e = tk.Entry(root, textvariable=amt)
-e.grid(row=1, column=1, columnspan=3, sticky='WE', padx=5, pady=5)
+e = ttk.Entry(root, textvariable=amt)
+e.grid(row=1, column=1, columnspan=3, sticky='nsew', padx=5, pady=5)
 
-tk.Label(root, text='Input Convert From (USD,INR,EUR,GBP etc)').grid(row=2, column=0, columnspan=5)
+ttk.Label(root, text='Input Convert From (USD,INR,EUR,GBP etc)',font='Times').grid(row=2, column=0, columnspan=5)
 
-e = tk.Entry(root, textvariable=cf)
-e.grid(row=3, column=1, columnspan=3, sticky='WE', padx=5, pady=5)
+e = ttk.Entry(root, textvariable=cf)
+e.grid(row=3, column=1, columnspan=3, sticky='NSEW', padx=5, pady=5)
 
-tk.Label(root, text='Input Convert To (USD,INR,EUR,GBP etc)').grid(row=4, column=0, columnspan=5)
+ttk.Label(root, text='Input Convert To (USD,INR,EUR,GBP etc)',font='Times').grid(row=4, column=0, columnspan=5)
 
-e = tk.Entry(root, textvariable=ct)
-e.grid(row=5, column=1, columnspan=3, sticky='WE', padx=5, pady=5)
+e = ttk.Entry(root, textvariable=ct)
+e.grid(row=5, column=1, columnspan=3, sticky='nsew', padx=5, pady=5)
 
 
-b = tk.Button(root, text='Convert', command=lambda r=1.08: show_tax(r))
-b.grid(row=7, column=2, padx=5, pady=5)
+b = ttk.Button(root, text='Convert', command=lambda r=1.08: show_tax(r))
+b.grid(row=7, column=2, padx=5, pady=5,sticky='nsew')
 
 
 # An empty Label to force row to be displayed
-tk.Label(root).grid(row=9, column=0, columnspan=5)
+ttk.Label(root).grid(row=9, column=0, columnspan=5)
 
-tk.Label(root, text='Converted Amount %s').grid(row=10, column=2, columnspan=2, sticky='WE')
+ttk.Label(root, text='--Converted Amount--').grid(row=10, column=2, columnspan=4, sticky='nsew')
 
-l = tk.Label(root, textvariable=net, relief='raised')
-l.grid(row=11, column=2, columnspan=2, sticky='WE')
+l = ttk.Label(root, textvariable=net, relief='raised')
+l.grid(row=11, column=1, columnspan=4, sticky='nsew')
 
 
 root.mainloop()
